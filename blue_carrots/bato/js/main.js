39f52e33decb9e5adda167f119585fb712e43a7e
anurag thapad <thapad973@gmail.com>
2019-07-07 13:00:39 +0530

    Dashboard and Services section ready

--- blue_carrots/bato/js/main.js
+++ blue_carrots/bato/js/main.js
@@ -1,17 +1,17 @@
 ;(function () {
-	
+
 	'use strict';
 
 
 
-	// iPad and iPod detection	
+	// iPad and iPod detection
 	var isiPad = function(){
 		return (navigator.platform.indexOf("iPad") != -1);
 	};
 
 	var isiPhone = function(){
 	    return (
-			(navigator.platform.indexOf("iPhone") != -1) || 
+			(navigator.platform.indexOf("iPhone") != -1) ||
 			(navigator.platform.indexOf("iPod") != -1)
 	    );
 	};
@@ -50,7 +50,7 @@
 		$('.animate-box').waypoint( function( direction ) {
 
 			if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-				
+
 				i++;
 
 				$(this.element).addClass('item-animate');
@@ -69,9 +69,9 @@
 							el.removeClass('item-animate');
 						},  k * 200, 'easeInOutExpo' );
 					});
-					
+
 				}, 100);
-				
+
 			}
 
 		} , { offset: '85%' } );
@@ -89,56 +89,56 @@
 	var counterWayPoint = function() {
 		if ($('#colorlib-counter').length > 0 ) {
 			$('#colorlib-counter').waypoint( function( direction ) {
-										
+
 				if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-					setTimeout( counter , 400);					
+					setTimeout( counter , 400);
 					$(this.element).addClass('animated');
 				}
 			} , { offset: '90%' } );
 		}
 	};
 
-	// Owl Carousel
-	var owlCarouselFeatureSlide = function() {
-		var owl2 = $('.owl-carousel');
-		owl2.owlCarousel({
-			animateOut: 'fadeOut',
-		   animateIn: 'fadeIn',
-		   autoplay: true,
-		   loop:true,
-		   margin:0,
-		   nav: true,
-		   dots: false,
-		   autoHeight: true,
-		   mouseDrag: false,
-		   autoplayHoverPause: true,
-		   items: 1,
-		   navText: [
-		      "<i class='icon-arrow-left3 owl-direction'></i>",
-		      "<i class='icon-arrow-right3 owl-direction'></i>"
-	     	]
-		});
-		var owl3 = $('.owl-carousel3');
-		owl3.owlCarousel({
-			animateOut: 'fadeOut',
-		   animateIn: 'fadeIn',
-		   autoplay: true,
-		   loop:true,
-		   margin:0,
-		   nav: false,
-		   dots: true,
-		   autoHeight: true,
-		   mouseDrag: false,
-		   autoplayHoverPause: true,
-		   items: 1,
-		   navText: [
-		      "<i class='icon-arrow-left3 owl-direction'></i>",
-		      "<i class='icon-arrow-right3 owl-direction'></i>"
-	     	]
-		});	
-	};
+	// // Owl Carousel
+	// var owlCarouselFeatureSlide = function() {
+	// 	var owl2 = $('.owl-carousel');
+	// 	owl2.owlCarousel({
+	// 		animateOut: 'fadeOut',
+	// 	   animateIn: 'fadeIn',
+	// 	   autoplay: true,
+	// 	   loop:true,
+	// 	   margin:0,
+	// 	   nav: true,
+	// 	   dots: false,
+	// 	   autoHeight: true,
+	// 	   mouseDrag: false,
+	// 	   autoplayHoverPause: true,
+	// 	   items: 1,
+	// 	   navText: [
+	// 	      "<i class='icon-arrow-left3 owl-direction'></i>",
+	// 	      "<i class='icon-arrow-right3 owl-direction'></i>"
+	//      	]
+	// 	});
+	// 	var owl3 = $('.owl-carousel3');
+	// 	owl3.owlCarousel({
+	// 		animateOut: 'fadeOut',
+	// 	   animateIn: 'fadeIn',
+	// 	   autoplay: true,
+	// 	   loop:true,
+	// 	   margin:0,
+	// 	   nav: false,
+	// 	   dots: true,
+	// 	   autoHeight: true,
+	// 	   mouseDrag: false,
+	// 	   autoplayHoverPause: true,
+	// 	   items: 1,
+	// 	   navText: [
+	// 	      "<i class='icon-arrow-left3 owl-direction'></i>",
+	// 	      "<i class='icon-arrow-right3 owl-direction'></i>"
+	//      	]
+	// 	});
+	// };
 
-	
+
 
 
 	// Document on load.
